ceb8ceffe39c198386030c93d3eaa4d6f3aa1ba0
Jan Heiland <jnhlnd@gmail.com>
2020-11-19 16:09:15 +0100

    here we call it index

--- mkdc.sh
+++ mkdc.sh
@@ -1,6 +1,6 @@
 MDFILE=slides.md
 
-pandoc $MDFILE -o slides.html \
+pandoc $MDFILE -o index.html \
     --mathjax --filter pandoc-citeproc -t revealjs --slide-level=2 -s \
     -V theme=solarized \
     -V viewDistance=15 -V width=1280 -V height=880 -V margin=0.05
